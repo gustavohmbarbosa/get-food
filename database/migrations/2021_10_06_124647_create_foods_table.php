81108b7864b29bb6102bd2c739b35ed5cc27709b
= <=>
2021-10-06 11:25:24 -0300

    Setup e criação das migrations

---
+++ database/migrations/2021_10_06_124647_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->float('price', 8, 2);
+            $table->dateTime('availability_start')->nullable();
+            $table->dateTime('availability_end')->nullable();
+            $table->boolean('always_available')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
